3d6b3b33b7ff05992ff4d2145b962615272f7953
PJGR <pgr2594@gmail.com>
2022-02-09 11:21:54 +0100

    Creada la rama PGR

--- src/proyecto_Git/ProyectoGit.java
+++ src/proyecto_Git/ProyectoGit.java
@@ -5,6 +5,9 @@ public class ProyectoGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Commit inicial");
+		System.out.println("creada rama PEDRO");
+		System.out.println("creada la rama PGR");
+
 	}
 
 }
